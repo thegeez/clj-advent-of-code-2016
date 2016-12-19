ec79c06a8c7679bc39fb38a1ad00d4421f4ce6c0
thegeez <thegeez@users.noreply.github.com>
2016-12-19 17:15:17 +0100

    dep for day 19

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject net.thegeez.advent-2016 "0.0.1"
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [digest "1.4.5"]]
+                 [digest "1.4.5"]
+
+                 [org.clojure/data.finger-tree "0.0.2"] ;; day19
+                 ]
 
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}})
